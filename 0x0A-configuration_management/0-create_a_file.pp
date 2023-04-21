c8e30817e8504376f700636cc2f399d8cad6b4dd
lavendahkinyua <lavendah17@gmail.com>
2023-04-21 09:46:15 -0700

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 #defining a file resource to create a file
 file { '/tmp/school':
   ensure => 'file',
-  mode => '0744',
   owner => 'www-data',
   group => 'www-data',
+  mode => '0744',
   content => 'I love Puppet',
 }
